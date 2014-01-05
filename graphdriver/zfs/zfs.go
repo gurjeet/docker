0908935d83b5395fe7d39116f738e5d6e9927237
Gurjeet Singh <gurjeet@singh.im>
2014-01-04 21:02:00 -0500

    Add code to remove a ZFS FS only after promoting one of its clones.

--- graphdriver/zfs/zfs.go
+++ graphdriver/zfs/zfs.go
@@ -21,10 +21,10 @@ func init() {
 }
 
 type Driver struct {
-	root string // Path to the root of the graph storage (as seen by Docker daemon)
-	root_dataset string // Name of the ZFS dataset mount at 'root'
+	root            string // Path to the root of the graph storage (as seen by Docker daemon)
+	root_dataset    string // Name of the ZFS dataset mount at 'root'
 	root_mountpoint string // Filesystem mountpoint; must be the same as 'root'
-	rand *rand.Rand
+	rand            *rand.Rand
 }
 
 /*
@@ -60,9 +60,9 @@ func Init(root string) (graphdriver.Driver, error) {
 	 * Split the output on tab characters.
 	 */
 	outSplice := strings.FieldsFunc(outStream,
-								func (r rune) bool {
-									return r == '\t'
-								})
+		func(r rune) bool {
+			return r == '\t'
+		})
 
 	/* No need to trim the trailing newline; execCmd() already did that. */
 	dataset_name := outSplice[0]
@@ -92,6 +92,7 @@ func (d *Driver) String() string {
 
 func (d *Driver) Status() [][2]string {
 	{funcName := funcEnter();defer funcLeave(funcName)}
+
 	return [][2]string{
 		{"Root Dir", d.root},
 		{"Dataset", d.root_dataset},
@@ -106,6 +107,7 @@ func (d *Driver) Status() [][2]string {
  */
 func (d *Driver) Cleanup() error {
 	{funcName := funcEnter();defer funcLeave(funcName)}
+
 	msg := "zfs-Cleanup: not yet implemented"
 	dbg(msg)
 	panic(msg)
@@ -178,16 +180,52 @@ func (d *Driver) Remove(id string) error {
 	dataset := d.getDataset(id)
 
 	/*
-	 * TODO: I need -R option during development because the cron jobs create
-	 * snapshots of filesystems automatically, and we can't destroy a filesystem
-	 * until all dependent objects are destroyd. Alternatively, use -d option to
-	 * "defer" the filesystem destruction when it's dependent objects are destroyed.
-	 * The -d option seems to be a safer choice for eventual code to be submitted.
+	 * Promote a clone, if any, of this dataset, and then remove the dataset. Other
+	 * snapshots and clones, if any, will automatically adjust to be dependent on
+	 * this promoted clone.
+	 *
+	 * There seems to be no need for us to promote a clone. I did some tests, and I
+	 * saw that Docker itself calls 'destroy' on container/image tree recursively,
+	 * and that it makes sure that it doesn't call 'destroy' on a filesystem until
+	 * all its children have been destroyed first. So as the code stands right now,
+	 * it is usable by Docker.
+	 *
+	 * But there may be some value to promoting a clone. For eg., if a user manually
+	 * creates a clone of a container's ZFS backing dataset, then upon container
+	 * deletion by Docker, we do `zfs destroy -R` and that will destroy the user's
+	 * manually-created snapshots and clones. In that case, promoting one of
+	 * user-created clones seems to be a safe choice. Since the code is already in
+	 * place, I'll just keep it around till final merge and see what Docker-core
+	 * team thinks of it.
 	 */
-	_, _, err := execCmd("zfs", "destroy", "-R", dataset)
+	outStream, _, err := execCmd("zfs", "list", "-r", "-H", "-o", "clones", "-t", "snapshot", dataset)
 	if err != nil {
 		return err // XXX We should cook a errors.New() with accurate message.
 	}
+
+	/*
+	 * Split the output on comma and newline characters.
+	 */
+	outSplice := strings.FieldsFunc(outStream,
+		func(r rune) bool {
+			return r == ',' || r == '\n'
+		})
+
+	if len(outSplice) != 0 {
+
+		clone_to_promote := outSplice[0]
+		_, _, err = execCmd("zfs", "promote", clone_to_promote)
+		if err != nil {
+			return err // XXX We should cook a errors.New() with accurate message.
+		}
+	}
+
+	/* Now destroy the container's FS, including any snapshots */
+	_, _, err = execCmd("zfs", "destroy", "-r", dataset)
+	if err != nil {
+		return err // XXX We should cook a errors.New() with accurate message.
+	}
+
 	return nil
 }
 
